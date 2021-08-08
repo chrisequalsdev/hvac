92a05f87cdacc3504acfeac1ef87f1a590ac154d
chris <christopher.bianchetti@gmail.com>
2021-08-08 13:43:08 -0700

    working

--- temp_v5_8266_ac/temp_v5_8266_ac.ino
+++ temp_v5_8266_ac/temp_v5_8266_ac.ino
@@ -10,20 +10,40 @@ OneWire oneWire(oneWireBus);
 // Pass our oneWire reference to Dallas Temperature sensor 
 DallasTemperature sensors(&oneWire);
 
+// Relay setup on D1
+int relay = 5;
+boolean relay_status = false;
+
 void setup() {
   // Start the Serial Monitor
   Serial.begin(115200);
   // Start the DS18B20 sensor
   sensors.begin();
+  // Init relay
+  pinMode(relay, OUTPUT);
 }
 
 void loop() {
   sensors.requestTemperatures(); 
   //float temperatureC = sensors.getTempCByIndex(0);
-  float temperatureF = sensors.getTempFByIndex(0);
+  float tempF = sensors.getTempFByIndex(0);
   //Serial.print(temperatureC);
   //Serial.println(" C");
-  Serial.print(temperatureF);
+  Serial.print(tempF);
   Serial.println(" F");
+
+  // Temp control logic
+  if (tempF >= 75.5) {
+    digitalWrite(relay, HIGH);
+    relay_status = true; 
+  }
+  else if (tempF >= 75 && tempF < 75.5 && relay_status == true) {
+    digitalWrite(relay, HIGH);
+  }
+  else {
+    digitalWrite(relay, LOW);
+    relay_status = false;      
+  }
+  
   delay(10000);
 }
